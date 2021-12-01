c747c8d3f244bb433a031b73ee563c1d55e0f537
Brandonisernest <48809099+Brandonisernest@users.noreply.github.com>
2021-12-01 14:04:17 -0500

    Add gameOrder() and gameHandler()

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -2,28 +2,52 @@
 1. loop through each cell
 2. assign eventlistener to each one
 */
+
 //global variables
 const gameCells = document.querySelectorAll(".cell");
-let playerChoices = ["X","Y"];
+const playerChoices = ["X", "Y"];
 let player;
+const gameBtn = document.getElementById("game-btn");
 //
 
 function cellHandler(cell) {
-    cell.innerHTML = 'BYE';
+  cell.innerHTML = player;
 }
-
-gameCells.forEach((cell)=> {
-    cell.addEventListener("click", cellHandler.bind(this,cell));
-})
 
 function gameStart() {
-    //randomize starting player
+  //randomize starting player
+  if (getRandomInt(2) == 0) {
+    player = playerChoices[0];
+  } else {
+    player = playerChoices[1];
+  }
+  //clear the board
+  gameCells.forEach((cell) => {
+    cell.innerHTML = "";
+  });
 }
 
+function gameOrder() {
+  if (player == playerChoice[0]) {
+    player = playerchoice[1];
+  } else {
+    player = playerChoice[0];
+  }
+}
+
+function gameHandler() {
+  gameCells.forEach((cell) => {
+    cell.addEventListener("click", cellHandler.bind(this, cell));
+  });
+}
 //helper function
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
+
+//execute functions
+gameBtn.addEventListener("click", gameStart);
+gameHandler();
 
 //appendix
 // gameCells.forEach((cell)=> {
@@ -33,6 +57,8 @@ function getRandomInt(max) {
 // })
 
 //testing
-for (let i = 0; i < 100; i++){
-    console.log(getRandomInt(2));
-}
+// for (let i = 0; i < 100; i++){
+//     console.log(getRandomInt(2));
+// }
+
+//Ask Henry: How to arrow functions work? why do they remove the need to "bind"?
