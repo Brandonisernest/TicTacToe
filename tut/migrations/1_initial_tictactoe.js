f890e459fbc6c44c9feed5ebe396ba59c24cb4be
Brandon <brandonisernest@gmail.com>
2021-12-03 12:25:05 -0500

    Added constructor inputs to deploy

--- tut/migrations/1_initial_tictactoe.js
+++ tut/migrations/1_initial_tictactoe.js
@@ -2,5 +2,5 @@
 const TicTacToe = artifacts.require("TicTacToe");
 
 module.exports = function (deployer) {
-  deployer.deploy(TicTacToe);
+  deployer.deploy(TicTacToe, { value: web3.utils.toWei("1","ether"), gas: 5000000});
 };
