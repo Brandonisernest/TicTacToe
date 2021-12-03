34e747d30cee2f271cbd098de6e2e495311af7b7
Brandon <brandonisernest@gmail.com>
2021-12-03 12:00:34 -0500

    Add clickCell logic

--- tut/contracts/TicTacToe.sol
+++ tut/contracts/TicTacToe.sol
@@ -8,6 +8,11 @@ contract TicTacToe {
     //X = 0, Y = 1
     enum possibleChoices {X, Y}
     //game grid (X,Y) coord
+    /*
+    One_One == 0,
+    Two_One == 1...etc etc
+    */
+
     enum cellChoices {One_One, 
                      Two_One, 
                      Three_One, 
@@ -16,9 +21,8 @@ contract TicTacToe {
                      Three_Two,
                      One_Three, 
                      Two_Three, 
-                     Three_Three}
-
-
+                     Three_Three
+                     }
 
 
     //variables
@@ -58,40 +62,78 @@ contract TicTacToe {
         playerFundMapping[msg.sender] = 0;
         //player's record is set to zero
         playerRecordMapping[msg.sender] = 0;
-
-
     }
-    function clickCell(possibleChoices _choice, string memory _rowNum, string memory _colNum) public {
+
+
+    /// @param _choice is X or Y
+    function clickCell(possibleChoices _choice, uint _rowNum, uint _colNum) public {
         //require player to be in contract first
         require(playerBoolMapping[msg.sender] == true, "You aren't eligible to play. Enter contract first!");
+
+        //set spot on the grid to target
+        cellChoices currentCellChoice;
+        currentCellChoice = cellHandler(_rowNum, _colNum);
+
+        //require the gameBoard space be empty
+        require(gameBoard[currentCellChoice] != possibleChoices.X && gameBoard[currentCellChoice] != possibleChoices.Y, 
+        "That space is already taken");
         
-        //concat _rownNum and _colNum into one string
-        string memory cellNum = cellNumber(_rowNum, _colNum);
+        gameBoard[currentCellChoice] = _choice;
     }
 
     //helper function
 
     function cellHandler(uint _rowNum, uint _colNum) public pure returns(cellChoices) {
+        require(_rowNum > 0 && _rowNum <= 3 && _colNum > 0 &&_colNum <= 3, "keep row and col 3x3");
         cellChoices currentCellChoice;
 
         if(_rowNum == 1 && _colNum == 1) {
             currentCellChoice = cellChoices.One_One;
         }
-
+        else if(_rowNum == 2 && _colNum == 1) {
+            currentCellChoice = cellChoices.Two_One;
+        }
+        else if(_rowNum == 3 && _colNum == 1) {
+            currentCellChoice = cellChoices.Three_One;
+        }
+        else if(_rowNum == 1 && _colNum == 2) {
+            currentCellChoice = cellChoices.One_Two;
+        }
+        else if(_rowNum == 2 && _colNum == 2) {
+            currentCellChoice = cellChoices.Two_Two;
+        }
+        else if(_rowNum == 3 && _colNum == 2) {
+            currentCellChoice = cellChoices.Three_Two;
+        }
+        else if(_rowNum == 1 && _colNum == 3) {
+            currentCellChoice = cellChoices.One_Three;
+        }
+        else if(_rowNum == 2 && _colNum == 3) {
+            currentCellChoice = cellChoices.Two_Three;
+        }
+        else if(_rowNum == 3 && _colNum == 3) {
+            currentCellChoice = cellChoices.Three_Three;
+        }
+    
         return currentCellChoice;
-
     }
 
     //concats numbers to strings (shall I make this "cellNumber" eventually?)..keep sep for now
-    function append(string memory a, string memory b) internal pure returns (string memory) {
-        return string(abi.encodePacked(a, b));
-    }
+    // function append(string memory a, string memory b) internal pure returns (string memory) {
+    //     return string(abi.encodePacked(a, b));
+    // }
 
-    //given row number outputs to _cellNumber
-    function cellNumber(string memory _rowNum, string memory _colNum) public view returns(string memory) {
-        return append(_rowNum, _colNum); 
-    }
+    // //given row number outputs to _cellNumber
+    // function cellNumber(string memory _rowNum, string memory _colNum) public view returns(string memory) {
+    //     return append(_rowNum, _colNum); 
+    // }
     
 }
 
 //Right now I am basing this on strings for easier readibility
+/*
+
+Read up on this (pure vs view)
+https://www.google.com/search?q=pure+vs+view+function+solidity&oq=pure+vs+view+function+solidity&aqs=chrome..69i57.4375j0j1&sourceid=chrome&ie=UTF-8
+
+*/
