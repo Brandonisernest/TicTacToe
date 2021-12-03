2eb53c3a359334d73a8d0cffe4a0f4dc5f00b064
Brandon <brandonisernest@gmail.com>
2021-12-03 13:11:22 -0500

    add more unit tests

--- tut/test/tic_tac_toe.js
+++ tut/test/tic_tac_toe.js
@@ -6,10 +6,7 @@ const TicTacToe = artifacts.require("TicTacToe");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 
-
-
 contract("TicTacToe", function (accounts) {
-
   let tttInstance;
 
   beforeEach(async () => {
@@ -22,23 +19,79 @@ contract("TicTacToe", function (accounts) {
     return assert.isTrue(true);
   });
 
-  // Test cell clicking ("X")
-  it("Should result in cell equalling X", async() => {
-    //function param (character, row_num, col_num)
-    let rowVal = 1;
-    let colVal = 3;
-    const cellVal = await TicTacToe.clickCell("X", rowVal, colVal);
-    const gameBoardVal = await TicTacToe.checkGameBoard(rowVal, colVal);
-    return assert.equal(gameBoardVal, "X");
+  // Test enter contract
+
+  it("should allow me to enter contract", async () => {
+    await tttInstance.enterContract({
+      from: accounts[1],
+      value: web3.utils.toWei("1", "Wei"),
+      gas: 5000000,
+    });
+
+    return assert.isTrue(true);
   });
 
-  // // Test cell clicking("Y")
-  // it("Should result in cell equalling Y", async() => {
-  //   //function param (character, row_num, col_num)
-  //   const cellVal = await TicTacToe.clickCell("Y", 3, 1);
-  //   return assert.equal(cellVal, ("Y", 3, 1));
+  // Test cell clicking ("X")
+  it("Should result in cell equalling X", async () => {
+    let rowVal = 1;
+    let colVal = 3;
+    let choiceVal = 1;
 
-  // });
+    //function param (character, row_num, col_num)
+    await tttInstance.clickCell(choiceVal, rowVal, colVal, {
+      from: accounts[1],
+      gas: 5000000,
+    });
+
+    const gameBoardVal = await tttInstance.checkGameBoard(rowVal, colVal);
+    // console.log(gameBoardVal);
+    //I'm using enum
+    //1 == Y
+    //2 == Y
+    return assert.equal(gameBoardVal, 1);
+  });
+
+  // Test cell clicking("Y")
+  it("Should result in cell equalling Y", async () => {
+    let rowVal = 2;
+    let colVal = 3;
+    let choiceVal = 2;
+
+    //function param (character, row_num, col_num)
+    await tttInstance.clickCell(choiceVal, rowVal, colVal, {
+      from: accounts[1],
+      gas: 5000000,
+    });
+
+    const gameBoardVal = await tttInstance.checkGameBoard(rowVal, colVal);
+    return assert.equal(gameBoardVal, 2);
+  });
+
+  // Test cannot select cell if cell is already taken up
+
+  it("Should not let you selected cell that is already taken up", async() => {
+
+    //these should be already taken in the "it" statement above
+    let rowVal = 2;
+    let colVal = 3;
+    let choiceVal = 1;
+
+
+    try{
+      //this should fail
+      await tttInstance.clickCell(choiceVal, rowVal, colVal, {
+        from: accounts[1],
+        gas: 5000000,
+      });
+    }
+    catch(err) {
+      console.log(`${rowVal} and ${colVal} is already taken...`);
+      return assert.isTrue(true);
+    }
+    
+
+
+  })
 
   // // Test the location of the cell
 
@@ -47,6 +100,4 @@ contract("TicTacToe", function (accounts) {
   //   await TicTacToe.winCondition();
   //   return assert.isTrue(true);
   // });
-
-  
 });
